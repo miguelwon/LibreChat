129d6c0f7257c5e3b183469b31f9c3a605b31a9a
Miguel <miguelwon@gmail.com>
2025-07-06 16:01:43 +0000

    add back expiredAt filter

--- api/models/Conversation.js
+++ api/models/Conversation.js
@@ -171,6 +171,8 @@ module.exports = {
       filters.push({ tags: { $in: tags } });
     }
 
+    filters.push({ $or: [{ expiredAt: null }, { expiredAt: { $exists: false } }] });
+
     if (search) {
       try {
         const meiliResults = await Conversation.meiliSearch(search);
